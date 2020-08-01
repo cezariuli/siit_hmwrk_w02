5d596691e3d3134d805a2dff0f26c25341375a62
cezariuli <cezariuli@gmail.com>
2020-08-01 15:01:31 +0300

    Started making the first steps in JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+/**
+Week 02 (Intro to Java) homework of  JAVA course.
+
+ Requirements:
+
+ Implement in java the following algorithms:
+
+ 1. Calculate the sum of the first 100 numbers higher than 0
+
+ 2. Display the smallest number from an array of number
+
+ 3. Display the max digit from a number.
+
+ 4. Check if a number is palindrom( e.g palindrom 1221, 34143)
+
+ 5. Display all the prime numbers lower than a given number
+
+Created: Aug 1, 2020
+ @author cezar
+*/
+
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Filip! This is Cezar's homework. " +
+                "Please select which of the below algorithms you want to review: \n" +
+                "1. Calculate the sum of the first 100 numbers higher than 0\n" +
+                "2. Display the smallest number from an array of number\n" +
+                "3. Display the max digit from a number.\n" +
+                "4. Check if a number is palindrom\n" +
+                "5. Display all the prime numbers lower than a given number\n");
+    }
 }
